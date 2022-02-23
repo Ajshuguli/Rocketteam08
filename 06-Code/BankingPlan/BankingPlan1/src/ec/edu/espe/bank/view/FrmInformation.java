8e0f0e032840f134d8060b16e0f960668b0d6e99
jgsimbana <gusjs84@gmail.com>
2022-02-23 00:18:01 -0500

    Proyect

--- 06-Code/BankingPlan/BankingPlan1/src/ec/edu/espe/bank/view/FrmInformation.java
+++ 06-Code/BankingPlan/BankingPlan1/src/ec/edu/espe/bank/view/FrmInformation.java
@@ -1,4 +1,7 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
 package ec.edu.espe.bank.view;
 
 /**
@@ -23,60 +26,192 @@ public class FrmInformation extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        txtGuide = new javax.swing.JTextArea();
-        btnReturn = new javax.swing.JButton();
-
-        jMenuItem1.setText("jMenuItem1");
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        jLabel17 = new javax.swing.JLabel();
+        jLabel18 = new javax.swing.JLabel();
+        jLabel19 = new javax.swing.JLabel();
+        jLabel20 = new javax.swing.JLabel();
+        jLabel21 = new javax.swing.JLabel();
+        jLabel22 = new javax.swing.JLabel();
+        jLabel23 = new javax.swing.JLabel();
+        jLabel24 = new javax.swing.JLabel();
+        jLabel16 = new javax.swing.JLabel();
+        jLabel25 = new javax.swing.JLabel();
+        jLabel26 = new javax.swing.JLabel();
+        jLabel27 = new javax.swing.JLabel();
+        jLabel28 = new javax.swing.JLabel();
+        jLabel29 = new javax.swing.JLabel();
+        jLabel30 = new javax.swing.JLabel();
+        jLabel33 = new javax.swing.JLabel();
+        jLabel34 = new javax.swing.JLabel();
+        jLabel31 = new javax.swing.JLabel();
+        jLabel32 = new javax.swing.JLabel();
+        jLabel35 = new javax.swing.JLabel();
+        bnReturn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        txtGuide.setEditable(false);
-        txtGuide.setBackground(new java.awt.Color(204, 204, 255));
-        txtGuide.setColumns(20);
-        txtGuide.setForeground(new java.awt.Color(51, 51, 51));
-        txtGuide.setRows(5);
-        txtGuide.setText("                                                       \n\n                                                                  Welcome to Banking Plan\n\nBanking Plan is  a program that helps to improve the mismanagement of a person's money\nfrom the control of their expenses so as to avoid losing money or even going bankrupt.\n\nBanking Plan will contribute to society since it is a system that will help us to administer\nmoney correctly. The program is a banking simulation where you will know the amount of\nmoney you have in your account, thus separating it into: Savings Account, Checking Account, and\ntheir respective interest generated monthly and annually.\n\n    How to register ?\n\n   1)  Enter age\n   2)  Enter User Name\n   3)  You need to create an id\n   4)  You need to create a password\n   5)  Click on register\n \n\n     How to enter ?\n\n    1)  Enter User Name\n    2)  Enter your password\n    3)  Click on Loig\n\n   Do you need to cancel  ?\n\n      Click on Cancel\n\n\n  Accounts\n                                                             Savings Account\n\nTo open your account you need to register. \nIn this account you can save your money and have it at the time you require\n\n                                                           Checking Account\n\nTo open your account you need to register.\nTo request this account you need a specific amount of money\n\n\n                                                                     Cdt.\n\nTo open your account you need to register.\n\n");
-        jScrollPane1.setViewportView(txtGuide);
+        jPanel1.setBackground(new java.awt.Color(204, 204, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        btnReturn.setText("Return");
-        btnReturn.addActionListener(new java.awt.event.ActionListener() {
+        jLabel1.setFont(new java.awt.Font("Tahoma", 3, 11)); // NOI18N
+        jLabel1.setText("Welcome to Banking Plan");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 10, 170, 30));
+
+        jLabel3.setText("Banking Plan is  a program that helps to improve the mismanagement of a person's");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 140, 500, 30));
+
+        jLabel4.setText("from the control of their expenses so as to avoid losing money or even going bankrupt.");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 160, 580, 20));
+
+        jLabel5.setText("Banking Plan will contribute to society since it is a system that will help us to administer");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 184, 530, 20));
+
+        jLabel6.setText("money correctly. The program is a banking simulation where you will know the amount of");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, 550, -1));
+
+        jLabel7.setText("money you have in your account, thus separating it into: Savings Account, Checking Account,  ");
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 214, 650, 20));
+
+        jLabel8.setText("and their respective interest generated monthly and annually.");
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, 370, -1));
+
+        jLabel9.setFont(new java.awt.Font("Tahoma", 3, 11)); // NOI18N
+        jLabel9.setText("How to register ?");
+        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 110, -1));
+
+        jLabel10.setText("   1)  Enter age");
+        jPanel1.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 290, -1, -1));
+
+        jLabel11.setText("2)  Enter User Name");
+        jPanel1.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 330, -1, -1));
+
+        jLabel12.setText("3)  You need to create an id");
+        jPanel1.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 370, -1, -1));
+
+        jLabel13.setText("4)  You need to create a password");
+        jPanel1.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 410, -1, -1));
+
+        jLabel14.setText("5)  Click on register");
+        jPanel1.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 460, -1, -1));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/GUIA.PNG"))); // NOI18N
+        jLabel2.setText("jLabel2");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 40, 180, 100));
+
+        jLabel15.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/NAME.PNG"))); // NOI18N
+        jPanel1.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 340, 260, 20));
+
+        jLabel17.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/ID.PNG"))); // NOI18N
+        jPanel1.add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 380, -1, 20));
+
+        jLabel18.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/CONTRA.PNG"))); // NOI18N
+        jPanel1.add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 430, -1, 30));
+
+        jLabel19.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/R.PNG"))); // NOI18N
+        jPanel1.add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 470, -1, 30));
+
+        jLabel20.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/AGE.PNG"))); // NOI18N
+        jPanel1.add(jLabel20, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 290, 240, 20));
+
+        jLabel21.setFont(new java.awt.Font("Tahoma", 3, 11)); // NOI18N
+        jLabel21.setText("How to enter ?");
+        jPanel1.add(jLabel21, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 530, 90, -1));
+
+        jLabel22.setText("1)  Enter User Name");
+        jPanel1.add(jLabel22, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 570, -1, -1));
+
+        jLabel23.setText("2)  Enter your password");
+        jPanel1.add(jLabel23, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 610, -1, -1));
+
+        jLabel24.setText("    3)  Click on Loig");
+        jPanel1.add(jLabel24, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 650, -1, -1));
+
+        jLabel16.setFont(new java.awt.Font("Tahoma", 3, 11)); // NOI18N
+        jLabel16.setText("Accounts");
+        jPanel1.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 30, 60, -1));
+
+        jLabel25.setText("To open your account you need to register. ");
+        jPanel1.add(jLabel25, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 210, -1, 20));
+
+        jLabel26.setText("In this account you can save your money and have it at the time you require");
+        jPanel1.add(jLabel26, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 240, -1, -1));
+
+        jLabel27.setFont(new java.awt.Font("Tahoma", 3, 11)); // NOI18N
+        jLabel27.setText("Savings Account");
+        jPanel1.add(jLabel27, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 60, 100, 20));
+
+        jLabel28.setFont(new java.awt.Font("Tahoma", 3, 11)); // NOI18N
+        jLabel28.setText("Checking Account");
+        jPanel1.add(jLabel28, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 280, 110, -1));
+
+        jLabel29.setText("To open your account you need to register.");
+        jPanel1.add(jLabel29, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 450, -1, -1));
+
+        jLabel30.setText("To request this account you need a specific amount of money");
+        jPanel1.add(jLabel30, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 470, -1, -1));
+
+        jLabel33.setFont(new java.awt.Font("Tahoma", 3, 11)); // NOI18N
+        jLabel33.setText("CDT");
+        jPanel1.add(jLabel33, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 500, 30, -1));
+
+        jLabel34.setText("To open your account you need to register.");
+        jPanel1.add(jLabel34, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 670, -1, -1));
+
+        jLabel31.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/SAVING.PNG"))); // NOI18N
+        jLabel31.setText("jLabel31");
+        jPanel1.add(jLabel31, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 90, 210, 110));
+
+        jLabel32.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/CHEC.PNG"))); // NOI18N
+        jPanel1.add(jLabel32, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 310, 250, 130));
+
+        jLabel35.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ec/edu/espe/bank/view/CDT.PNG"))); // NOI18N
+        jPanel1.add(jLabel35, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 520, 140, -1));
+
+        bnReturn.setText("Return");
+        bnReturn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnReturnActionPerformed(evt);
+                bnReturnActionPerformed(evt);
             }
         });
+        jPanel1.add(bnReturn, new org.netbeans.lib.awtextra.AbsoluteConstraints(960, 680, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 519, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnReturn)
-                .addContainerGap())
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 323, Short.MAX_VALUE)
-                .addGap(5, 5, 5)
-                .addComponent(btnReturn))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 715, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnReturnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnReturnActionPerformed
-         FrmAccount open = new FrmAccount ();
+    private void bnReturnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bnReturnActionPerformed
+       FrmAccount open = new FrmAccount();
         open.setVisible(true);
-        this.setVisible(false);
-    }//GEN-LAST:event_btnReturnActionPerformed
+        this.setVisible (false);
+    }//GEN-LAST:event_bnReturnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -114,9 +249,42 @@ public class FrmInformation extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnReturn;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea txtGuide;
+    private javax.swing.JButton bnReturn;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel18;
+    private javax.swing.JLabel jLabel19;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel20;
+    private javax.swing.JLabel jLabel21;
+    private javax.swing.JLabel jLabel22;
+    private javax.swing.JLabel jLabel23;
+    private javax.swing.JLabel jLabel24;
+    private javax.swing.JLabel jLabel25;
+    private javax.swing.JLabel jLabel26;
+    private javax.swing.JLabel jLabel27;
+    private javax.swing.JLabel jLabel28;
+    private javax.swing.JLabel jLabel29;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel30;
+    private javax.swing.JLabel jLabel31;
+    private javax.swing.JLabel jLabel32;
+    private javax.swing.JLabel jLabel33;
+    private javax.swing.JLabel jLabel34;
+    private javax.swing.JLabel jLabel35;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
